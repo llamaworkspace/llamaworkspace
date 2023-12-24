a329e3e7eaaa16f7698635efc564638bb46dc410
Isaac Mayolas <imayolas@gmail.com>
2023-12-24 16:07:49 +0100

    move file

--- src/pages/api/status.ts
+++ src/pages/api/status.ts
@@ -1,3 +1,3 @@
-import { statusCheckerHandler } from '@/server/chats/handlers/statusChecker/statusCheckerHandler'
+import { statusCheckerHandler } from '@/server/global/handlers/statusChecker/statusCheckerHandler'
 
 export default statusCheckerHandler
