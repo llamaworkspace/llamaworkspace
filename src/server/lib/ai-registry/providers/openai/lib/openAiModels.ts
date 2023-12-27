694e2b23df9da6dfbd509118717de35530e8ac99
Isaac Mayolas <imayolas@gmail.com>
2023-12-27 16:01:02 +0100

    Fixes bug with gpt4 (#9)

--- src/server/lib/ai-registry/providers/openai/lib/openAiModels.ts
+++ src/server/lib/ai-registry/providers/openai/lib/openAiModels.ts
@@ -2,5 +2,8 @@ import type { AiRegistryModel } from '../../../aiRegistryTypes'
 
 export const openAiModels: AiRegistryModel[] = [
   { slug: 'gpt-3.5-turbo' as const, publicName: 'Chat GPT 3.5 Turbo' as const },
-  { slug: 'gpt-4' as const, publicName: 'Chat GPT-4 special edition' as const },
+  {
+    slug: 'gpt-4-1106-preview' as const,
+    publicName: 'Chat GPT-4 Turbo' as const,
+  },
 ]
