aa0fa75948ba2e758746899db505f17b13a70c86
Isaac Mayolas <imayolas@gmail.com>
2024-01-02 18:35:33 +0100

    group call in a service

--- src/components/ai/backend/procedures/getAiProviders.ts
+++ src/components/ai/backend/procedures/getAiProviders.ts
@@ -1,8 +1,5 @@
-import { maskValueWithBullets } from '@/lib/appUtils'
-import { aiRegistry } from '@/server/ai/aiRegistry'
-import { getAiProvidersKVs } from '@/server/ai/services/getProvidersForWorkspace.service'
+import { getAiProvidersWithKVs } from '@/server/ai/services/getProvidersForWorkspace.service'
 import { protectedProcedure } from '@/server/trpc/trpc'
-import _ from 'underscore'
 import { z } from 'zod'
 
 const zInput = z.object({
@@ -14,35 +11,10 @@ export const getAiProviders = protectedProcedure
   .query(async ({ ctx, input }) => {
     const userId = ctx.session.user.id
 
-    const providersMeta = aiRegistry.getProvidersMeta()
-    const aiProvidersKVBySlug = await getAiProvidersKVs(
+    return await getAiProvidersWithKVs(
       ctx.prisma,
       input.workspaceId,
       userId,
+      true,
     )
-
-    return providersMeta.map((providerMeta) => {
-      const providerSlug = providerMeta.slug
-      const provider = aiRegistry.getProvider(providerSlug)
-      const providerFields = provider?.fields ?? []
-
-      const fieldSlugs = providerFields.map((field) => field.slug)
-
-      // Pick only Provider-registered fields
-      const providerKV = _.chain(aiProvidersKVBySlug[providerSlug])
-        .pick(...fieldSlugs)
-        .mapObject((value: string, key) => {
-          const field = providerFields.find((field) => field.slug === key)
-          if (field?.encrypted) {
-            return maskValueWithBullets(value)
-          }
-          return value
-        })
-        .value()
-
-      return {
-        ...providerMeta,
-        values: providerKV ?? {},
-      }
-    })
   })
