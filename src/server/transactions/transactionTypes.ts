5aee323f142ff32865c991714d09c700f5a7e17c
Isaac Mayolas <imayolas@gmail.com>
2024-01-18 14:29:17 +0100

    remove server file

--- src/server/transactions/transactionTypes.ts
+++
@@ -1,9 +0,0 @@
-export enum TrxAccount {
-  WorkspaceBalance = 'workspace_balance',
-  UserCreditsUse = 'user_credits_use',
-}
-
-export enum TrxType {
-  Debit = 'debit', // From / Left side of the equation
-  Credit = 'credit', // To / Right side of the equation
-}
