9050e776ab0f5742e4432f45298595d291c11925
Isaac Mayolas <imayolas@gmail.com>
2024-02-07 19:04:39 +0100

    Revert title generation to gpt4 (#53)

--- src/shared/aiTypesAndMappers.ts
+++ src/shared/aiTypesAndMappers.ts
@@ -13,10 +13,6 @@ export enum ChatAuthor {
 
 export enum OpenAiModelEnum {
   GPT3_5_TURBO = 'openai/gpt-3.5-turbo',
-  GPT4 = 'openai/gpt-4-turbo-preview',
-}
-
-export const OpenaiModelToHuman: Record<OpenAiModelEnum, string> = {
-  [OpenAiModelEnum.GPT3_5_TURBO]: 'ChatGPT 3.5 Turbo',
-  [OpenAiModelEnum.GPT4]: 'ChatGPT 4',
+  GPT4 = 'openai/gpt-4',
+  GPT4_TURBO = 'openai/gpt-4-turbo-preview',
 }
