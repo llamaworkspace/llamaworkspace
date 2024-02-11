b3c93b4ee492047b9202b93dc520d19b486adb81
Isaac Mayolas <imayolas@gmail.com>
2024-02-11 16:26:41 +0100

    poc

--- src/components/chats/components/ChatHeaderPostTitle.tsx
+++ src/components/chats/components/ChatHeaderPostTitle.tsx
@@ -5,6 +5,7 @@ import { Editable } from '@/components/ui/Editable'
 import { Skeleton } from '@/components/ui/skeleton'
 import { DEFAULT_API_DEBOUNCE_MS } from '@/shared/globalConfig'
 import { PermissionAction } from '@/shared/permissions/permissionDefinitions'
+import EmojiPicker, { Emoji } from 'emoji-picker-react'
 
 export const ChatHeaderPostTitle = ({ postId }: { postId?: string }) => {
   const { data: post, isLoading } = usePostById(postId)
@@ -28,15 +29,36 @@ export const ChatHeaderPostTitle = ({ postId }: { postId?: string }) => {
   }
 
   return (
-    <div className="flex w-full items-center gap-x-4 text-zinc-900">
-      <Editable
-        onChange={handleTitleChange}
-        tagName="h1"
-        className="text-lg font-semibold tracking-tighter"
-        placeholder={EMPTY_POST_NAME}
-        initialValue={post?.title}
-        disabled={!canEdit}
-      />
+    <div className="relative">
+      <div className="flex w-full items-center gap-x-1 text-zinc-900">
+        <div className="bg-zinc-100x relative px-2 py-1 text-xl">
+          <div>
+            <Emoji unified="1f423" size="25" />
+          </div>
+          <div>
+            <div className="hiddenx absolute left-0 top-9 z-50">
+              <EmojiPicker
+                style={{
+                  '--epr-emoji-fullsize': '12px',
+                  '--epr-emoji-size': '12px',
+                }}
+                open={true}
+                width={300}
+                previewConfig={{ showPreview: false }}
+                searchDisabled
+              />
+            </div>
+          </div>
+        </div>
+        <Editable
+          onChange={handleTitleChange}
+          tagName="h1"
+          className="text-lg font-semibold tracking-tighter"
+          placeholder={EMPTY_POST_NAME}
+          initialValue={post?.title}
+          disabled={!canEdit}
+        />
+      </div>
     </div>
   )
 }
