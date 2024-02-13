e86e27cf26979e63c2f3a71b50c6124183056445
Isaac Mayolas <imayolas@gmail.com>
2024-02-13 22:33:18 +0100

    Revert "Revert "Chat style change - Grayscale" (#58)"
    This reverts commit d185bb3321e6797e32b7cff9c7f596ff48fb037b.

--- src/components/chats/Chat.tsx
+++ src/components/chats/Chat.tsx
@@ -46,7 +46,7 @@ export function Chat({ postId, chatId }: ChatProps) {
       key={refreshKey}
       className="relative flex h-full w-full flex-1 flex-col-reverse overflow-y-auto overflow-x-hidden bg-white p-4"
     >
-      <div className="mx-auto flex w-full max-w-4xl grow flex-col gap-y-2 pb-4">
+      <div className="mx-auto flex w-full max-w-4xl grow flex-col gap-y-8 pb-4">
         <div className="grow">
           <ChatNoSettingsAlert postId={postId} chatId={chatId} />
         </div>
@@ -96,9 +96,9 @@ function getAuthor(author: string) {
 
 function getVariant(author: string) {
   if (author === (ChatAuthor.Assistant as string)) {
-    return 'green'
+    return 'assistant'
   } else if (author === (ChatAuthor.Wizard as string)) {
     return 'wizard'
   }
-  return 'default'
+  return 'user'
 }
