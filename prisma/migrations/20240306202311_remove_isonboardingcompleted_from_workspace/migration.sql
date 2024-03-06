1d724b87c2510d6a5f9e910aa36b0c8ce25794cf
Isaac Mayolas <imayolas@gmail.com>
2024-03-06 21:23:20 +0100

    remove from db

---
+++ prisma/migrations/20240306202311_remove_isonboardingcompleted_from_workspace/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `isOnboardingCompleted` on the `Workspace` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Workspace" DROP COLUMN "isOnboardingCompleted";
