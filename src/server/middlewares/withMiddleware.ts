42b8c9f6aa8d544c0c0d5ac94e5f6d47e67ece66
Isaac Mayolas <imayolas@gmail.com>
2024-03-12 12:48:04 +0100

    Chore/next 14 polishes (#92)

--- src/server/middlewares/withMiddleware.ts
+++ src/server/middlewares/withMiddleware.ts
@@ -1,12 +1,30 @@
 import { label } from 'next-api-middleware'
+import type { NextRequest, NextResponse } from 'next/server'
 import { apiCaptureErrorsMiddleware } from './nextjs/apiCaptureErrorsMiddleware'
+import { apiCaptureErrorsMiddlewareForAppRouter } from './nextjs/apiCaptureErrorsMiddlewareForAppRouter'
 
 // Todo: Check all the libraries in lib/ and see if they need to be updated to use this.
-const withMiddleware = label(
+export const withMiddleware = label(
   {
     sentry: apiCaptureErrorsMiddleware,
   },
   ['sentry'],
 )
 
-export { withMiddleware }
+type HandlerFunc = (
+  request: Request | NextRequest,
+  response?: Response | NextResponse,
+) => Promise<unknown>
+
+export const withMiddlewareForAppRouter = (handler: HandlerFunc) => {
+  return async (
+    request: Request | NextRequest,
+    response?: Response | NextResponse,
+  ) => {
+    try {
+      return await handler(request, response)
+    } catch (error) {
+      return apiCaptureErrorsMiddlewareForAppRouter(error as Error)
+    }
+  }
+}
