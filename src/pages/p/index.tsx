3363b8df42f5151075943d2cd35a3e3493e3c97e
Isaac Mayolas <imayolas@gmail.com>
2024-03-14 11:21:38 +0100

    Redirect

--- src/pages/p/index.tsx
+++ src/pages/p/index.tsx
@@ -1,5 +1,21 @@
-import { GlobalRedirector } from '@/components/global/components/GlobalRedirector'
+import { MainLayout } from '@/components/layout/MainLayout'
+import { HeaderVariants } from '@/components/layout/MainLayout/MainLayoutHeader'
+import { useCurrentWorkspace } from '@/components/workspaces/workspacesHooks'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 export default function AppIndexPage() {
-  return <GlobalRedirector />
+  const { replace } = useRouter()
+  const { workspace } = useCurrentWorkspace()
+
+  useEffect(() => {
+    async function doRedirect() {
+      if (!workspace) return
+      const url = `/c/new?workspaceId=${workspace.id}`
+      await replace(url)
+    }
+    void doRedirect()
+  }, [workspace, replace])
+
+  return <MainLayout variant={HeaderVariants.Hidden} />
 }
---
+++ src/server/posts/services/getDefaultPost.service.ts
@@ -0,0 +1,28 @@
+import type { UserOnWorkspaceContext } from '@/server/auth/userOnWorkspaceContext'
+import type { PrismaClientOrTrxClient } from '@/shared/globalTypes'
+import { scopePostByWorkspace } from '../postUtils'
+import { createDefaultPostService } from './createDefaultPost.service'
+
+export const getDefaultPostService = async (
+  prisma: PrismaClientOrTrxClient,
+  uowContext: UserOnWorkspaceContext,
+) => {
+  const { userId, workspaceId } = uowContext
+
+  const post = await prisma.post.findFirst({
+    select: { id: true },
+    where: scopePostByWorkspace(
+      {
+        userId,
+        isDefault: true,
+      },
+      workspaceId,
+    ),
+  })
+  // This scenario should never happen, but this is just
+  // defensive code to generate a default post as a last resort
+  if (!post) {
+    return await createDefaultPostService(prisma, workspaceId, userId)
+  }
+  return post
+}
