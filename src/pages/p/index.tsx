ccf12f5cc384589f436ce68d5475a5e5111d087f
Isaac Mayolas <imayolas@gmail.com>
2024-03-20 13:10:29 +0100

    fix entrypiint

--- src/pages/p/index.tsx
+++ src/pages/p/index.tsx
@@ -1,12 +1,9 @@
 import { MainLayout } from '@/components/layout/MainLayout'
 import { HeaderVariants } from '@/components/layout/MainLayout/MainLayoutHeader'
-import { useCurrentWorkspace } from '@/components/workspaces/workspacesHooks'
 import { getServerAuthSession } from '@/server/auth/nextauth'
 import { prisma } from '@/server/db'
 import { getEntrypointRedirectUrl } from '@/server/global/services/getEntrypointRedirectUrl.service'
 import type { GetServerSideProps, GetServerSidePropsContext } from 'next'
-import { useRouter } from 'next/router'
-import { useEffect } from 'react'
 
 export const getServerSideProps: GetServerSideProps = async (
   context: GetServerSidePropsContext,
@@ -23,7 +20,7 @@ export const getServerSideProps: GetServerSideProps = async (
   const userId = session?.user.id
 
   const { url } = await getEntrypointRedirectUrl(prisma, userId)
-  console.log('url', url)
+
   return {
     redirect: {
       destination: url,
@@ -33,17 +30,5 @@ export const getServerSideProps: GetServerSideProps = async (
 }
 
 export default function AppIndexPage() {
-  const { replace } = useRouter()
-  const { workspace } = useCurrentWorkspace()
-
-  useEffect(() => {
-    async function doRedirect() {
-      if (!workspace) return
-      const url = `/c/new?workspaceId=${workspace.id}`
-      await replace(url)
-    }
-    // void doRedirect()
-  }, [workspace, replace])
-  return <div>work in progress</div>
   return <MainLayout variant={HeaderVariants.Hidden} />
 }
