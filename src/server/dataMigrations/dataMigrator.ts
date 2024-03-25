d9f1411aed36740c526d29e46349aae6fcdef9d7
Isaac Mayolas <imayolas@gmail.com>
2024-03-25 23:22:01 +0100

    file fiex

--- src/server/dataMigrations/dataMigrator.ts
+++ src/server/dataMigrations/dataMigrator.ts
@@ -1,12 +1,13 @@
 import { env } from '@/env.mjs'
+import path from 'path'
 import { Sequelize } from 'sequelize'
 import { SequelizeStorage, Umzug } from 'umzug'
+
 const sequelize = new Sequelize(env.DATABASE_URL)
 
 export const migrator = new Umzug({
   migrations: {
-    // glob: ['data_migrations/*.ts', { cwd: path.join(__dirname, '../../..') }],
-    glob: ['data_migrations/*.ts', { cwd: process.cwd() }],
+    glob: ['data_migrations/*.ts', { cwd: path.join(__dirname, '../../..') }],
   },
   storage: new SequelizeStorage({ sequelize, tableName: '_data_migrations' }),
   logger: console,
