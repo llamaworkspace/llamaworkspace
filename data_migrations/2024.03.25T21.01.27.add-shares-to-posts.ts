954ad95fc2223388e635220bd9a213c6546ecb68
Isaac Mayolas <imayolas@gmail.com>
2024-03-25 23:18:22 +0100

    add data migration

---
+++ data_migrations/2024.03.25T21.01.27.add-shares-to-posts.ts
@@ -0,0 +1,20 @@
+import { prisma } from '@/server/db'
+import { ShareScope } from '@/shared/globalTypes'
+import type { MigrationFn } from 'umzug'
+
+export const up: MigrationFn = async () => {
+  const posts = await prisma.post.findMany({
+    select: {
+      id: true,
+    },
+  })
+  await prisma.share.createMany({
+    data: posts.map((post) => ({
+      postId: post.id,
+      scope: ShareScope.Everybody,
+    })),
+  })
+}
+export const down: MigrationFn = async () => {
+  await prisma.share.deleteMany()
+}
