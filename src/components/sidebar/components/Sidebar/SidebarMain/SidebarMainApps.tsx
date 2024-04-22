9c3e6d92bd9f90cf90635a9564756a7c694fc5d5
Isaac Mayolas <imayolas@gmail.com>
2024-04-22 15:39:41 +0200

    progress

--- src/components/sidebar/components/Sidebar/SidebarMain/SidebarMainApps.tsx
+++ src/components/sidebar/components/Sidebar/SidebarMain/SidebarMainApps.tsx
@@ -1,7 +1,9 @@
 import { useCreateSharedChat } from '@/components/chats/chatHooks'
 import { EMPTY_POST_NAME } from '@/components/posts/postsConstants'
+import { useCreatePost } from '@/components/posts/postsHooks'
 import { usePostsForSidebar } from '@/components/sidebar/sidebarHooks'
 import { useCurrentWorkspace } from '@/components/workspaces/workspacesHooks'
+import { useNavigation } from '@/lib/frontend/useNavigation'
 import { cn } from '@/lib/utils'
 import { Square3Stack3DIcon } from '@heroicons/react/24/outline'
 import { Emoji } from 'emoji-picker-react'
@@ -9,25 +11,27 @@ import { Emoji } from 'emoji-picker-react'
 interface AppItemProps {
   postId: string
   title: string | null
-  highlighted?: boolean
 }
 
-const AppItem = ({ postId, title, highlighted = true }: AppItemProps) => {
+const AppItem = ({ postId, title }: AppItemProps) => {
+  const navigation = useNavigation()
   const { mutate: createChat } = useCreateSharedChat()
+  const isActive = navigation.query.post_id === postId
 
   return (
     <div>
       <div
         className={cn(
           'flex w-full cursor-pointer items-center gap-2 rounded px-2 py-2 text-[14px] font-bold text-zinc-950 transition hover:bg-zinc-200/80 active:bg-zinc-300',
-          highlighted && 'bg-zinc-950 text-white',
-          !highlighted && 'text-zinc-950',
+          isActive && 'bg-zinc-200',
         )}
         onClick={() => createChat({ postId })}
       >
         <span className="flex grow basis-0 items-center gap-x-1">
           <Emoji unified={'2728'} size={24} />
-          {title ? title : EMPTY_POST_NAME}
+          <span className="line-clamp-1">
+            {title ? title : EMPTY_POST_NAME}
+          </span>
         </span>
       </div>
     </div>
@@ -35,8 +39,18 @@ const AppItem = ({ postId, title, highlighted = true }: AppItemProps) => {
 }
 
 const AppExploreItem = () => {
+  const { data: workspace } = useCurrentWorkspace()
+  const { mutate: createPost } = useCreatePost()
+
+  const handleCreatePost = () => {
+    if (!workspace?.id) return
+    createPost({ workspaceId: workspace.id })
+  }
   return (
-    <div className="flex w-full cursor-pointer items-center gap-2 rounded px-2 py-2 text-[14px] font-bold text-zinc-950 transition hover:bg-zinc-200/80">
+    <div
+      className="flex w-full cursor-pointer items-center gap-2 rounded px-2 py-2 text-[14px] font-bold text-zinc-950 transition hover:bg-zinc-200/80"
+      onClick={handleCreatePost}
+    >
       <span className="flex grow basis-0 items-center gap-x-1 ">
         <Square3Stack3DIcon className="h-6 w-6" />
         Explore GPTs
@@ -50,7 +64,7 @@ export const SidebarMainApps = () => {
   const { sortedPosts } = usePostsForSidebar(workspace?.id)
 
   return (
-    <div className="space-y-1">
+    <div className="space-y-0.5">
       {sortedPosts?.map((post) => {
         return <AppItem key={post.id} postId={post.id} title={post.title} />
       })}
