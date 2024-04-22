962b11ee4a09fbef870345b32ba63df559769de0
Isaac Mayolas <imayolas@gmail.com>
2024-04-22 16:23:47 +0200

    modify new chat button

--- src/components/sidebar/components/Sidebar/SidebarHeader.tsx
+++ src/components/sidebar/components/Sidebar/SidebarHeader.tsx
@@ -1,3 +1,4 @@
+import { cn } from '@/lib/utils'
 import { PencilSquareIcon } from '@heroicons/react/24/outline'
 
 export function SidebarHeader() {
@@ -13,10 +14,14 @@ const NewChatButton = () => {
     <div className="flex max-h-[48px] ">
       <button
         onClick={() => console.log(11)}
-        className="flex grow basis-0 items-center justify-start gap-x-2 rounded px-2 py-3 font-bold text-zinc-950 transition-with-margin hover:bg-zinc-200/80 active:m-0.5 active:bg-zinc-200/80 active:duration-75 active:ease-in "
+        className="group flex grow basis-0 items-center justify-between gap-x-2 rounded-lg border-zinc-100 bg-zinc-100 px-3 py-3 text-[14px] font-bold text-zinc-950 shadow transition-with-margin hover:bg-zinc-200/80 active:m-0.5 active:bg-zinc-200/80 active:duration-75 active:ease-in "
       >
-        <PencilSquareIcon className="h-5 w-5 text-zinc-950" />
         <span>New chat</span>
+        <PencilSquareIcon
+          className={cn(
+            'h-6 w-6 text-zinc-400 transition group-hover:block group-hover:text-zinc-950',
+          )}
+        />
       </button>
     </div>
   )
