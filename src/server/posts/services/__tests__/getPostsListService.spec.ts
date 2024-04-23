4af63b81637c5b963841fe5279c123fec2b16a34
Isaac Mayolas <imayolas@gmail.com>
2024-04-23 11:21:40 +0200

    Add test

---
+++ src/server/posts/services/__tests__/getPostsListService.spec.ts
@@ -0,0 +1,55 @@
+import { createUserOnWorkspaceContext } from '@/server/auth/userOnWorkspaceContext'
+import { prisma } from '@/server/db'
+import { PostFactory } from '@/server/testing/factories/PostFactory'
+import { UserFactory } from '@/server/testing/factories/UserFactory'
+import { WorkspaceFactory } from '@/server/testing/factories/WorkspaceFactory'
+import type { Post, Share, User, Workspace } from '@prisma/client'
+import { getPostsListService } from '../getPostsList.service'
+
+const subject = async (userId: string, workspaceId: string) => {
+  const context = await createUserOnWorkspaceContext(
+    prisma,
+    workspaceId,
+    userId,
+  )
+  return await getPostsListService(prisma, context)
+}
+
+describe('getPostsListService', () => {
+  let workspace: Workspace
+  let user: User
+  let sharedUser: User
+  let post1: Post
+  let post2: Post
+  let share: Share
+
+  beforeEach(async () => {
+    workspace = await WorkspaceFactory.create(prisma)
+
+    user = await UserFactory.create(prisma, {
+      workspaceId: workspace.id,
+    })
+
+    post1 = await PostFactory.create(prisma, {
+      userId: user.id,
+      workspaceId: workspace.id,
+    })
+
+    post2 = await PostFactory.create(prisma, {
+      userId: user.id,
+      workspaceId: workspace.id,
+    })
+  })
+
+  it('returns the posts', async () => {
+    const result = await subject(user.id, workspace.id)
+
+    expect(result).toHaveLength(2)
+    expect(result).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining(post1),
+        expect.objectContaining(post2),
+      ]),
+    )
+  })
+})
--- src/server/posts/services/getPostsList.service.ts
+++ src/server/posts/services/getPostsList.service.ts
@@ -1,13 +1,17 @@
 import type { UserOnWorkspaceContext } from '@/server/auth/userOnWorkspaceContext'
 import type { PrismaClientOrTrxClient } from '@/shared/globalTypes'
+import { scopePostByWorkspace } from '../postUtils'
 
 export const getPostsListService = async function (
   prisma: PrismaClientOrTrxClient,
   uowContext: UserOnWorkspaceContext,
 ) {
   return await prisma.post.findMany({
-    where: {
-      isDefault: false,
-    },
+    where: scopePostByWorkspace(
+      {
+        isDefault: false,
+      },
+      uowContext.workspaceId,
+    ),
   })
 }
