704f6fa13548f73a834d5e00f2b5ca44659c312f
Isaac Mayolas <imayolas@gmail.com>
2024-04-24 15:39:05 +0200

    Mark active chat in sidebar

--- src/components/sidebar/components/Sidebar/SidebarMain/SidebarMainChatHistory/SidebarMainChatHistoryTimeBlock.tsx
+++ src/components/sidebar/components/Sidebar/SidebarMain/SidebarMainChatHistory/SidebarMainChatHistoryTimeBlock.tsx
@@ -1,4 +1,5 @@
 import type { RouterOutputs } from '@/lib/api'
+import { useNavigation } from '@/lib/frontend/useNavigation'
 import { SidebarMainChatHistoryItem } from './SidebarMainChatHistoryItem'
 
 type ChatHistoryForSidebarOutput =
@@ -13,6 +14,9 @@ export const SidebarMainChatHistoryTimeBlock = ({
   title,
   chats,
 }: ChatHistoryTimeBlockProps) => {
+  const navigation = useNavigation()
+  const isIndividualChatPage = navigation.pathname === '/c/[chat_id]'
+
   return (
     <div className="w-full space-y-1">
       <div className="px-2 text-xs font-bold uppercase text-zinc-400">
@@ -25,6 +29,9 @@ export const SidebarMainChatHistoryTimeBlock = ({
               key={chat.id}
               chatId={chat.id}
               title={chat.title}
+              isActive={
+                isIndividualChatPage && navigation.query.chat_id === chat.id
+              }
             />
           )
         })}
