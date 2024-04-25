1541c01cbcc39aab3ac9124895421e2841eeb601
Isaac Mayolas <imayolas@gmail.com>
2024-04-25 15:57:44 +0200

    text fix

--- src/components/sidebar/components/Sidebar/SidebarMain/SidebarMainApps/SidebarMainExploreButton.tsx
+++ src/components/sidebar/components/Sidebar/SidebarMain/SidebarMainApps/SidebarMainExploreButton.tsx
@@ -11,7 +11,7 @@ export const SidebarMainExploreButton = () => {
 
   return (
     <SidebarMainItemShell
-      title="See all GPTs"
+      title="All GPTs"
       isActive={isActive}
       showPencil={false}
       icon={<Square3Stack3DIcon className="h-6 w-6" />}
