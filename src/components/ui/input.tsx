031e77f1af9533225ac47dd35092e0734234a948
Isaac Mayolas <imayolas@gmail.com>
2024-05-07 09:51:14 +0200

    standarize input

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,12 +1,12 @@
 import { cn, mergeRefs } from '@/lib/utils'
-import { VariantProps, cva } from 'class-variance-authority'
+import { cva, type VariantProps } from 'class-variance-authority'
 import { forwardRef, useEffect, useRef } from 'react'
 
 const inputVariants = cva('', {
   variants: {
     variant: {
       default:
-        'flex h-9 w-full rounded-md border border-zinc-200 bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-zinc-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-zinc-950 disabled:cursor-not-allowed disabled:opacity-50 dark:border-zinc-800 dark:placeholder:text-zinc-400 dark:focus-visible:ring-zinc-300',
+        'flex h-9 w-full rounded-md border border-zinc-200 bg-transparent px-3 py-1 shadow-sm transition-colors file:border-0 file:bg-transparent file:font-medium placeholder:text-zinc-400 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-zinc-950 disabled:cursor-not-allowed disabled:opacity-50 dark:border-zinc-800 dark:placeholder:text-zinc-400 dark:focus-visible:ring-zinc-300',
       unstyled: '',
     },
   },
