a8664f56a014e155eb68aacef538ee0b266e625b
Isaac Mayolas <imayolas@gmail.com>
2024-05-07 21:10:18 +0200

    handle title change

--- src/components/chats/components/ChatHeaderPostTitle.tsx
+++ src/components/chats/components/ChatHeaderPostTitle.tsx
@@ -1,40 +1,11 @@
-import { useCanExecuteActionForPost } from '@/components/permissions/permissionsHooks'
 import { EMPTY_POST_NAME } from '@/components/posts/postsConstants'
-import { usePostById, useUpdatePost } from '@/components/posts/postsHooks'
+import { usePostById } from '@/components/posts/postsHooks'
 import { JoiaIcon24 } from '@/components/ui/icons/JoiaIcon'
 import { Skeleton } from '@/components/ui/skeleton'
-import { DEFAULT_API_DEBOUNCE_MS } from '@/shared/globalConfig'
-import { PermissionAction } from '@/shared/permissions/permissionDefinitions'
-import { Emoji, type EmojiClickData } from 'emoji-picker-react'
-import { useState } from 'react'
+import { Emoji } from 'emoji-picker-react'
 
 export const ChatHeaderPostTitle = ({ postId }: { postId?: string }) => {
   const { data: post, isLoading } = usePostById(postId)
-  const [isEmojiEditable, setIsEmojiEditable] = useState(false)
-
-  const { mutate: updatePost } = useUpdatePost(DEFAULT_API_DEBOUNCE_MS)
-
-  const { can: canEdit } = useCanExecuteActionForPost(
-    PermissionAction.Update,
-    postId,
-  )
-
-  const handleSelectEmoji = (emoji: EmojiClickData) => {
-    handleEmojiChange(emoji.unified)
-    setIsEmojiEditable(false)
-  }
-
-  const handleEmojiChange = (emoji: string) => {
-    if (!postId) return
-    updatePost({ id: postId, emoji })
-  }
-
-  const handleTitleChange = (title: string) => {
-    title = title.trim()
-
-    if (!postId) return
-    updatePost({ id: postId, title: title || null })
-  }
 
   if (isLoading) {
     return <Skeleton className="h-5 w-96" />
