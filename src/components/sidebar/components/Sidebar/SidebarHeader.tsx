14c90471a05310efa2b010c2340ffbfa79e16d2e
Isaac Mayolas <imayolas@gmail.com>
2024-05-10 11:18:35 +0200

    new chat button align

--- src/components/sidebar/components/Sidebar/SidebarHeader.tsx
+++ src/components/sidebar/components/Sidebar/SidebarHeader.tsx
@@ -4,7 +4,7 @@ import { PencilSquareIcon } from '@heroicons/react/24/outline'
 
 export function SidebarHeader() {
   return (
-    <div className="p-2">
+    <div className="flex h-14 max-h-14 w-full items-center px-2">
       <NewChatButton />
     </div>
   )
@@ -13,7 +13,7 @@ export function SidebarHeader() {
 const NewChatButton = () => {
   const { mutate: createChat } = useCreateStandaloneChat()
   return (
-    <div className="flex max-h-[45px]">
+    <div className="flex max-h-[45px] w-full">
       <button
         onClick={() => createChat()}
         className="group flex grow basis-0 items-center justify-between gap-x-2 rounded-lg bg-zinc-100 px-2 py-3 text-[14px] font-bold text-zinc-950 transition-with-margin hover:bg-zinc-200 active:m-0.5 active:bg-zinc-200 active:duration-75 active:ease-in "
