16693a73c074fef5b24aa4b4e7e5b0b547c53b96
Isaac Mayolas <imayolas@gmail.com>
2024-05-13 12:10:26 +0200

    deprecate component

--- src/components/sidebar/backend/sidebarRouter.ts
+++ src/components/sidebar/backend/sidebarRouter.ts
@@ -1,10 +1,10 @@
 import { createTRPCRouter } from '@/server/trpc/trpc'
 import { getChatHistoryForSidebar } from './procedures/getChatHistoryForSidebar'
-import { getInfoCardForSidebar } from './procedures/getInfoCardForSidebar'
+import { deprecated_getInfoCardForSidebar } from './procedures/getInfoCardForSidebar'
 import { getPostsForSidebar } from './procedures/getPostsForSidebar'
 
 export const sidebarRouter = createTRPCRouter({
   postsForSidebar: getPostsForSidebar,
   chatHistoryForSidebar: getChatHistoryForSidebar,
-  infoCardForSidebar: getInfoCardForSidebar,
+  infoCardForSidebar: deprecated_getInfoCardForSidebar,
 })
