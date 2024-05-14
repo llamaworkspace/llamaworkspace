78a31c34c877de329b0dc5a2d3941cda76de645d
Isaac Mayolas <imayolas@gmail.com>
2024-05-14 10:36:11 +0200

    create a default postconfigversion in the post factory

--- src/server/testing/factories/PostFactory.ts
+++ src/server/testing/factories/PostFactory.ts
@@ -1,3 +1,5 @@
+import { Author } from '@/shared/aiTypesAndMappers'
+import { DEFAULT_AI_MODEL } from '@/shared/globalConfig'
 import { ShareScope, UserAccessLevel } from '@/shared/globalTypes'
 import { faker } from '@faker-js/faker'
 import type { Post, PrismaClient } from '@prisma/client'
@@ -47,6 +49,20 @@ export const PostFactory = {
         data: {
           workspaceId,
           shares: sharesPayload,
+          postConfigVersions: {
+            create: [
+              {
+                model: DEFAULT_AI_MODEL,
+                messages: {
+                  create: [
+                    {
+                      author: Author.System,
+                    },
+                  ],
+                },
+              },
+            ],
+          },
           ...PostFactory.build(rest),
         },
       })
