a1ad512d5d2593ba4028425c9e05b40e96634c2b
Isaac Mayolas <imayolas@gmail.com>
2024-05-19 14:34:41 +0200

    improved poc

--- src/runners/OpenAiAssistantRunner.ts
+++ src/runners/OpenAiAssistantRunner.ts
@@ -1,19 +1,49 @@
-import { env } from '@/env.mjs'
 import { AssistantResponse } from 'ai'
 import createHttpError from 'http-errors'
 import OpenAI from 'openai'
 import type { MessageCreateParams } from 'openai/resources/beta/threads/messages'
+import { SafeParseReturnType, z } from 'zod'
 
 const message = {
   role: 'user',
   content: 'What is the meaning of life?',
 } as MessageCreateParams
 
-const assistantId = 'asst_xlWStmqHYrJC5IsxQm6D0W8t'
+const constructorParams = z
+  .object({
+    apiKey: z.string(),
+    baseURL: z.string().optional(),
+  })
+  .strict()
 
-export class OpenaiAssistantRunner implements Strategy {
-  async stream() {
-    const openai = new OpenAI({ apiKey: env.INTERNAL_OPENAI_API_KEY })
+const streamParams = z
+  .object({
+    assistantId: z.string(),
+  })
+  .strict()
+
+type OpenAiAssistantStrategyConstructorParams = z.infer<
+  typeof constructorParams
+>
+type OpenAiAssistantStrategyStreamParams = z.infer<typeof streamParams>
+
+export class OpenaiAssistantStrategy
+  implements Strategy<OpenAiAssistantStrategyStreamParams>
+{
+  private readonly apiKey: string
+  private readonly baseURL: string | undefined
+
+  static validateConstructorParamsOrThrow(params: unknown) {
+    return constructorParams.parse(params)
+  }
+
+  constructor(params: OpenAiAssistantStrategyConstructorParams) {
+    this.apiKey = params.apiKey
+    this.baseURL = params.baseURL
+  }
+
+  async stream(payload: OpenAiAssistantStrategyStreamParams) {
+    const openai = new OpenAI({ apiKey: this.apiKey, baseURL: this.baseURL })
 
     const thread = await openai.beta.threads.create({})
     const threadId = thread.id
@@ -31,7 +61,7 @@ export class OpenaiAssistantRunner implements Strategy {
 
       async ({ forwardStream }) => {
         const runStream = openai.beta.threads.runs.stream(threadId, {
-          assistant_id: assistantId,
+          assistant_id: payload.assistantId,
         })
 
         // forward run status would stream message deltas
@@ -56,8 +86,101 @@ export class OpenaiAssistantRunner implements Strategy {
       },
     )
   }
+
+  validateStreamInputParams(params: OpenAiAssistantStrategyStreamParams) {
+    return streamParams.safeParse(params)
+  }
 }
 
-interface Strategy {
-  stream(): Promise<unknown>
+export const anotherStrategyParams = z.object({
+  pepe: z.string(),
+})
+
+const anotherStrategyStreamParams = z.object({
+  juan: z.string(),
+})
+
+type AnotherStrategyConstructorParams = z.infer<typeof anotherStrategyParams>
+type AnotherStrategyStreamParams = z.infer<typeof anotherStrategyStreamParams>
+
+export class AnotherAssistantStrategy
+  implements Strategy<AnotherStrategyStreamParams>
+{
+  private readonly apiKey: string
+  private readonly baseURL: string | undefined
+
+  constructor(params: AnotherStrategyConstructorParams) {
+    this.apiKey = params.apiKey
+    this.baseURL = params.baseURL
+  }
+
+  async stream(payload: AnotherStrategyStreamParams) {
+    const openai = new OpenAI({ apiKey: this.apiKey, baseURL: this.baseURL })
+
+    const thread = await openai.beta.threads.create({})
+    const threadId = thread.id
+
+    const createdMessage = await openai.beta.threads.messages.create(
+      threadId,
+      message,
+    )
+
+    return AssistantResponse(
+      {
+        threadId,
+        messageId: createdMessage.id,
+      },
+
+      async ({ forwardStream }) => {
+        const runStream = openai.beta.threads.runs.stream(threadId, {
+          assistant_id: payload.assistantId,
+        })
+
+        // forward run status would stream message deltas
+        const runResult = await forwardStream(runStream)
+        // runResult.status can be: queued, in_progress, requires_action, cancelling, cancelled, failed, completed, or expired
+
+        if (!runResult) {
+          throw new Error('Run result is undefined')
+        }
+
+        while (runResult.status === 'requires_action') {}
+
+        // if (['completed', 'cancelled'].includes(runResult.status)) {
+        //   onFinal(runResult)
+        // }
+
+        if (runResult.status === 'failed') {
+          const error = createHttpError(403, 'Run failed')
+          error.payload = runResult
+          // onError(error)
+        }
+      },
+    )
+  }
+
+  validateStreamInputParams(params: AnotherStrategyStreamParams) {
+    return anotherStrategyStreamParams.safeParse(params)
+  }
+}
+
+export class AssistantRunner<T> {
+  private readonly strategy: Strategy<T>
+  constructor(strategy: Strategy<T>) {
+    this.strategy = strategy
+  }
+
+  async stream(payload: T) {
+    const validation = this.strategy.validateStreamInputParams(payload)
+    // const validation = this.strategy.validateStreamInputParams({ pepe: true })
+    if (!validation.success) {
+      throw validation.error // Throws a zod error. Review is this is the desired behavior
+    }
+    return this.strategy.stream(payload)
+  }
+}
+
+export interface Strategy<T> {
+  stream(payload: T): Promise<unknown>
+  validateStreamInputParams(params: T): SafeParseReturnType<T, T>
 }
--- src/server/chats/handlers/chatStreamedResponse/chatStreamResponseHandlerV2.ts
+++ src/server/chats/handlers/chatStreamedResponse/chatStreamResponseHandlerV2.ts
@@ -1,4 +1,8 @@
-import { OpenaiAssistantRunner } from '@/runners/OpenAiAssistantRunner'
+import { env } from '@/env.mjs'
+import {
+  AssistantRunner,
+  OpenaiAssistantStrategy,
+} from '@/runners/OpenAiAssistantRunner'
 import type { NextRequest } from 'next/server'
 import { z } from 'zod'
 
@@ -26,11 +30,95 @@ const body = {
 
 // Request type=oai_assistant
 // Pick the right strategy based on the chatId
-// Bring the runtime parameters from the database
+// Bring the runtime parameters from the database (zod validate them)
 // Run the strategy, which will return a stream of messages
 
-export default async function chatStreamedResponseHandlerV2(req: NextRequest) {
-  const strategy = new OpenaiAssistantRunner()
+const type: 'another' | 'openai' = 'openai'
 
-  return strategy.stream()
+export default async function chatStreamedResponseHandlerV2(req: NextRequest) {
+  // const strategy = new AssistantStrategyResolver(prisma, [
+  //   OpenaiAssistantStrategy,
+  //   AnotherAssistantStrategy,
+  // ])
+
+  if (type === 'openai') {
+    const dbConstructorFields = await getDbConstructorFields()
+    OpenaiAssistantStrategy.validateConstructorParamsOrThrow(
+      dbConstructorFields,
+    ) // I should enforce this!!
+    const runnerStrategy = new OpenaiAssistantStrategy(dbConstructorFields)
+    const assistantRunner = new AssistantRunner(runnerStrategy)
+
+    // We fetch assistantId here. It's an internally created id, not exposed to the user
+    // Who generates this id in this example where there is no file upload phase?
+    // We could generate it in the creation of the gpt, or after each interaction with a getOrCreate.
+    // This getOrCreate should live in the runnerStrategy, and the runnerStrategy should have access to a kv store.
+
+    // In the UI I envision: Select app type? (a) GPT with function calling, (b) OpenAI assistant, (b) Custom RAG?. Then, for each type, we show what is possible: Function calling, twyd, etc.
+    // In the UI I envision: Select app type? (a) GPT with function calling, (b) OpenAI assistant, (b) Custom RAG?. Then, for each type, we show what is possible: Function calling, twyd, etc.
+    const dbRuntimeFields = await getDbRuntimeFields()
+    return await assistantRunner.stream(dbRuntimeFields)
+  } else {
+    // const runnerStrategy = new AnotherAssistantStrategy({ pepe: 'pepe' })
+    // const assistantRunner = new AssistantRunner(runnerStrategy)
+    // return await assistantRunner.stream({
+    //   juan: 'juan',
+    // })
+    // zod parse the dbFields
+    // const fields = anotherStrategyParams.parse(dbFields)
+    // runnerStrategy = new AnotherAssistantStrategy(fields)
+  }
+  throw new Error('Not implemented yet')
+  // const runnerStrategy = new OpenaiAssistantStrategy({
+  //   apiKey: env.INTERNAL_OPENAI_API_KEY,
+  // })
+
+  // const anotherStrategy = new AnotherAssistantStrategy({
+  //   pepe: 'pepe',
+  // })
+
+  // AssistantStrategyResolver
+
+  // const assistantRunner = new AssistantRunner(runnerStrategy)
+
+  // return await assistantRunner.stream({
+  //   assistantId: 'asst_xlWStmqHYrJC5IsxQm6D0W8t',
+  // })
+}
+
+// class AssistantStrategyResolver {
+//   constructor(
+//     private readonly prisma: PrismaClient,
+//     private readonly strategies: Strategy<unknown>[],
+//   ) {}
+
+//   async resolve(chatId: string) {
+//     const postType = await this.getPostType(chatId)
+//     return this.strategies[0]!
+//   }
+
+//   private async getPostType(chatId: string) {
+//     const chat = await this.prisma.chat.findUniqueOrThrow({
+//       where: { id: chatId },
+//       include: {
+//         post: true,
+//       },
+//     })
+
+//     return chat?.post.type
+//   }
+// }
+
+const getDbConstructorFields = async () => {
+  await Promise.resolve()
+  return {
+    apiKey: env.INTERNAL_OPENAI_API_KEY,
+  }
+}
+
+const getDbRuntimeFields = async () => {
+  await Promise.resolve()
+  return {
+    assistantId: 'asst_xlWStmqHYrJC5IsxQm6D0W8t',
+  }
 }
