d80af942fa6e5f7c34fc23159ee10473cb967eff
Isaac Mayolas <imayolas@gmail.com>
2024-05-20 10:05:51 +0200

    do migration

--- prisma/migrations/20240520075404_move_postonusers_to_apponusers/migration.sql
+++ prisma/migrations/20240520075404_move_postonusers_to_apponusers/migration.sql
@@ -1,20 +1,44 @@
+/*
+  Migration manually altered.
+  
+  Warnings:
 
--- RENAME TABLE
-ALTER TABLE "PostsOnUsers" RENAME TO "AppsOnUsers";
+  - You are about to drop the `PostsOnUsers` table. If the table is not empty, all the data it contains will be lost.
 
--- RENAME COLUMNS
-ALTER TABLE "AppsOnUsers" RENAME COLUMN "postId" TO "appId";
+*/
 
--- RENAME CONSTRAINTS
-ALTER TABLE "AppsOnUsers" RENAME CONSTRAINT "PostsOnUsers_pkey" TO "AppsOnUsers_pkey";
-ALTER TABLE "AppsOnUsers" RENAME CONSTRAINT "PostsOnUsers_postId_fkey" TO "AppsOnUsers_appId_fkey";
-ALTER TABLE "AppsOnUsers" RENAME CONSTRAINT "PostsOnUsers_userId_fkey" TO "AppsOnUsers_userId_fkey";
+-- CreateTable
+CREATE TABLE "AppsOnUsers" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "appId" TEXT NOT NULL,
+    "position" INTEGER,
+    "pinnedAt" TIMESTAMP(3),
+    "lastVisitedAt" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
+    CONSTRAINT "AppsOnUsers_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "AppsOnUsers_userId_appId_key" ON "AppsOnUsers"("userId", "appId");
+
+-- AddForeignKey
+ALTER TABLE "AppsOnUsers" ADD CONSTRAINT "AppsOnUsers_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "AppsOnUsers" ADD CONSTRAINT "AppsOnUsers_appId_fkey" FOREIGN KEY ("appId") REFERENCES "Post"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+
+-- Migrate data
+INSERT INTO "AppsOnUsers" ("id", "userId", "appId", "position", "pinnedAt", "lastVisitedAt", "createdAt", "updatedAt") SELECT "id", "userId", "postId", "position", "pinnedAt", "lastVisitedAt", "createdAt", "updatedAt" FROM "PostsOnUsers";
+
+-- DropForeignKey
 ALTER TABLE "PostsOnUsers" DROP CONSTRAINT "PostsOnUsers_postId_fkey";
 
--- RENAME INDEXES
-ALTER INDEX "PostsOnUsers_pkey" RENAME TO "AppsOnUsers_pkey";
-ALTER INDEX "PostsOnUsers_userId_postId_key" RENAME TO "AppsOnUsers_userId_appId_key";
+-- DropForeignKey
+ALTER TABLE "PostsOnUsers" DROP CONSTRAINT "PostsOnUsers_userId_fkey";
 
--- RENAME EVERYTHING ELSE
-ALTER SEQUENCE "PostsOnUsers_id_seq" RENAME TO "AppsOnUsers_id_seq";
+-- DropTable
+DROP TABLE "PostsOnUsers";
