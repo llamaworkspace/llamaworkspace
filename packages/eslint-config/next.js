ea14898065820c26759592934517a6c932d15bad
Isaac Mayolas <imayolas@gmail.com>
2024-05-26 10:33:05 +0200

    eslint adj

--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -1,35 +1,47 @@
-const { resolve } = require("node:path");
-
-const project = resolve(process.cwd(), "tsconfig.json");
-
 /** @type {import("eslint").Linter.Config} */
-module.exports = {
+const config = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: true,
+  },
+  plugins: ['@typescript-eslint'],
   extends: [
-    "eslint:recommended",
-    "prettier",
-    require.resolve("@vercel/style-guide/eslint/next"),
-    "eslint-config-turbo",
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'next',
+    'prettier',
+    'eslint-config-turbo',
   ],
-  globals: {
-    React: true,
-    JSX: true,
-  },
-  env: {
-    node: true,
-    browser: true,
-  },
-  plugins: ["only-warn"],
-  settings: {
-    "import/resolver": {
-      typescript: {
-        project,
+  rules: {
+    // These opinionated rules are enabled in stylistic-type-checked above.
+    // Feel free to reconfigure them to your own preference.
+    'react-hooks/exhaustive-deps': 'error', // or 'error' to enforce it as an error
+    '@typescript-eslint/array-type': 'off',
+    '@typescript-eslint/consistent-type-definitions': 'off',
+    '@typescript-eslint/consistent-type-imports': [
+      'warn',
+      {
+        prefer: 'type-imports',
+        fixStyle: 'inline-type-imports',
       },
-    },
+    ],
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'no-restricted-imports': [
+      'warn',
+      {
+        paths: [
+          {
+            name: 'next/router',
+            importNames: ['useRouter'],
+            message:
+              "Use `import { useNavigation } from '@/lib/frontend/useNavigation'` instead.",
+          },
+        ],
+      },
+    ],
+    '@typescript-eslint/no-non-null-asserted-optional-chain': 'off',
   },
-  ignorePatterns: [
-    // Ignore dotfiles
-    ".*.js",
-    "node_modules/",
-  ],
-  overrides: [{ files: ["*.js?(x)", "*.ts?(x)"] }],
-};
+}
+
+module.exports = config
