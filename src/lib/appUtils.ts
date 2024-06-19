b3d29a79cb9f550498ad7d570e2f50e528904864
Isaac Mayolas <imayolas@gmail.com>
2024-06-19 07:54:35 +0200

    move file

--- src/lib/appUtils.ts
+++
@@ -1,9 +0,0 @@
-export const maskValueWithBullets = (value: string) => {
-  if (!value) return value
-  const bullets = '••••••••••••••••'
-  if (value.length < 12) {
-    return bullets
-  }
-
-  return value?.slice(0, 4) + '••••••••••••••••' + value?.slice(-3)
-}
