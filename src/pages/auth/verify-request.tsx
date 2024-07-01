c65e5ae159bbe04aa18eb1a9d246e9711c6b73b9
Isaac Mayolas <imayolas@gmail.com>
2024-07-01 17:24:28 +0200

    interim

---
+++ src/pages/auth/verify-request.tsx
@@ -0,0 +1,3 @@
+export default function SignInPage() {
+  return <div>been here</div>
+}
--- src/server/auth/nextauth.ts
+++ src/server/auth/nextauth.ts
@@ -1,14 +1,19 @@
-import { handleUserSignup } from '@/components/auth/handleUserSignup'
+import { handleUserSignup } from '@/components/auth/backend/handleUserSignup'
 import { env } from '@/env.mjs'
 import { prisma } from '@/server/db'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import { type GetServerSidePropsContext } from 'next'
 import {
+  Theme,
   getServerSession,
   type DefaultSession,
   type NextAuthOptions,
 } from 'next-auth'
+import EmailProvider, {
+  SendVerificationRequestParams,
+} from 'next-auth/providers/email'
 import GoogleProvider from 'next-auth/providers/google'
+import { createTransport } from 'nodemailer'
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -41,6 +46,7 @@ export const authOptions: NextAuthOptions = {
   debug: false,
   pages: {
     signIn: '/auth/signin',
+    verifyRequest: '/auth/verify-request',
   },
   callbacks: {
     session({ session, user }) {
@@ -64,6 +70,11 @@ export const authOptions: NextAuthOptions = {
         },
       },
     }),
+    EmailProvider({
+      server: env.SMTP_EMAIL_SERVER,
+      from: env.SMTP_EMAIL_FROM,
+      sendVerificationRequest,
+    }),
   ],
 
   events: {
@@ -83,4 +94,81 @@ export const getServerAuthSession = (ctx: {
   res: GetServerSidePropsContext['res']
 }) => {
   return getServerSession(ctx.req, ctx.res, authOptions)
+}
+
+async function sendVerificationRequest(params: SendVerificationRequestParams) {
+  const { identifier, url, provider } = params
+
+  // Hack derived from callbackUrl actually existing in the provider object
+  const { callbackUrl } = provider as unknown as { callbackUrl: string }
+  if (!callbackUrl) {
+    throw new Error('Missing callbackUrl')
+  }
+  const { host } = new URL(url)
+  // NOTE: You are not required to use `nodemailer`, use whatever you want.
+  const transport = createTransport(provider.server)
+  const result = await transport.sendMail({
+    to: identifier,
+    from: provider.from,
+    subject: `Sign in to ${host}`,
+    text: textEmail({ callbackUrl }),
+    // html: html({ url, host, theme }),
+  })
+  const failed = result.rejected.concat(result.pending).filter(Boolean)
+  if (failed.length) {
+    throw new Error(`Email(s) (${failed.join(', ')}) could not be sent`)
+  }
+}
+
+function html(params: { url: string; host: string; theme: Theme }) {
+  const { url, host, theme } = params
+
+  const escapedHost = host.replace(/\./g, '&#8203;.')
+
+  const brandColor = theme.brandColor || '#346df1'
+  const color = {
+    background: '#f9f9f9',
+    text: '#444',
+    mainBackground: '#fff',
+    buttonBackground: brandColor,
+    buttonBorder: brandColor,
+    buttonText: theme.buttonText || '#fff',
+  }
+
+  return `
+<body style="background: ${color.background};">
+  <table width="100%" border="0" cellspacing="20" cellpadding="0"
+    style="background: ${color.mainBackground}; max-width: 600px; margin: auto; border-radius: 10px;">
+    <tr>
+      <td align="center"
+        style="padding: 10px 0px; font-size: 22px; font-family: Helvetica, Arial, sans-serif; color: ${color.text};">
+        1111Sign in to <strong>${escapedHost}</strong>
+      </td>
+    </tr>
+    <tr>
+      <td align="center" style="padding: 20px 0;">
+        <table border="0" cellspacing="0" cellpadding="0">
+          <tr>
+            <td align="center" style="border-radius: 5px;" bgcolor="${color.buttonBackground}"><a href="${url}"
+                target="_blank"
+                style="font-size: 18px; font-family: Helvetica, Arial, sans-serif; color: ${color.buttonText}; text-decoration: none; border-radius: 5px; padding: 10px 20px; border: 1px solid ${color.buttonBorder}; display: inline-block; font-weight: bold;">Sign
+                in</a></td>
+          </tr>
+        </table>
+      </td>
+    </tr>
+    <tr>
+      <td align="center"
+        style="padding: 0px 0px 10px 0px; font-size: 16px; line-height: 22px; font-family: Helvetica, Arial, sans-serif; color: ${color.text};">
+        If you did not request this email you can safely ignore it.
+      </td>
+    </tr>
+  </table>
+</body>
+`
+}
+
+/** Email Text body (fallback for email clients that don't render HTML, e.g. feature phones) */
+function textEmail({ callbackUrl }: { callbackUrl: string }) {
+  return `Sign in to ${callbackUrl}`
 }
