e6e127eef5c479aba4b75628d5be3484b5a0f9d3
Isaac Mayolas <imayolas@gmail.com>
2024-07-09 10:33:40 +0200

    Fix errors with doublequotes (#227)

--- src/lib/aiSdkUtils.ts
+++ src/lib/aiSdkUtils.ts
@@ -1,12 +1,19 @@
 import createHttpError from 'http-errors'
 
 export const generateAiSdkCompatibleErrorString = (error: Error) => {
+  // Make the string JSON safe; otherwise contents
+  // with double quotes will break the string
+
+  const stringified = JSON.stringify({ m: error.message })
+  // Use the stringified message to safely encode json quotes
+  const message = stringified.slice(6, stringified.length - 2).trim()
+
   if (createHttpError.isHttpError(error)) {
     if (error.status < 500) {
-      return `3:"::public::${error.message}"\n`
+      return `3:"::public::${message}"\n`
     }
   }
-  return `3:"${error.message}"\n`
+  return `3:"${message}"\n`
 }
 
 export const isAiSdkErrorString = (errorString: string) => {
