b38a1f62a04cd119cc910a9f1ebfd92d4526e2dc
Isaac Mayolas <imayolas@gmail.com>
2024-07-26 07:07:05 +0200

    style

--- src/pages/w/[workspace_id]/onboarding.tsx
+++ src/pages/w/[workspace_id]/onboarding.tsx
@@ -3,18 +3,24 @@ import Image from 'next/image'
 
 export default function Onboarding() {
   return (
-    <div className="mx-auto max-w-2xl space-y-4 px-4 py-12">
-      <Image
-        alt="Llama Workspace logo"
-        src="/images/llama-workspace-logo-black-square.svg"
-        width="64"
-        height="64"
-        className="self-center"
-      />
-      <div className="leading-nont text-xl font-semibold tracking-tight">
-        Let&apos;s get this party started
+    <div className="min-h-full bg-zinc-100 py-16">
+      <div className="mx-auto max-w-2xl space-y-4">
+        <div className="px-8 md:px-0">
+          <Image
+            alt="Llama Workspace logo"
+            src="/images/llama-workspace-logo-black-square.svg"
+            width="64"
+            height="64"
+            className="self-center"
+          />
+        </div>
+        <div className="space-y-4 rounded bg-white p-8">
+          <div className="leading-nont text-xl font-semibold tracking-tight">
+            Let&apos;s get this party started
+          </div>
+          <OnboardingScreen />
+        </div>
       </div>
-      <OnboardingScreen />
     </div>
   )
 }
