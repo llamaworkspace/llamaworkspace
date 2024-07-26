c62ec1a186c26965718e2ac89cd805b091ef96c9
Isaac Mayolas <imayolas@gmail.com>
2024-07-26 09:52:31 +0200

    redirect if ob is completed

--- src/pages/w/[workspace_id]/onboarding.tsx
+++ src/pages/w/[workspace_id]/onboarding.tsx
@@ -1,6 +1,53 @@
 import { OnboardingScreen } from '@/components/onboarding/components/OnboardingScreen'
 import { useNavigation } from '@/lib/frontend/useNavigation'
+import { getServerAuthSession } from '@/server/auth/nextauth'
+import { prisma } from '@/server/db'
+import type { GetServerSideProps, GetServerSidePropsContext } from 'next'
 import Image from 'next/image'
+
+export const getServerSideProps: GetServerSideProps = async (
+  context: GetServerSidePropsContext,
+) => {
+  const { query } = context
+  const { workspace_id } = query
+
+  const session = await getServerAuthSession(context)
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth/signin',
+        permanent: false,
+      },
+    }
+  }
+
+  const workspace = await prisma.workspace.findFirst({
+    where: {
+      id: workspace_id as string,
+      users: {
+        some: {
+          userId: session?.user.id,
+        },
+      },
+    },
+  })
+
+  if (!workspace) {
+    return {
+      redirect: {
+        destination: '/auth/signin',
+        permanent: false,
+      },
+    }
+  }
+
+  if (workspace?.onboardingCompletedAt) {
+    return { redirect: { destination: `/p`, permanent: false } }
+  }
+
+  return { props: {} }
+}
 
 export default function Onboarding() {
   const navigation = useNavigation()
