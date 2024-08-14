a02c56b3d96d122616b3aeed3d68c22b3b098013
Nicolas Casco <nico@llamaworkspace.ai>
2024-08-14 12:38:31 +0200

    change to pgvector

--- prisma/migrations/20240813140134_add_assetsembeddings/migration.sql
+++ prisma/migrations/20240813140134_add_assetsembeddings/migration.sql
@@ -1,16 +1,34 @@
+-- -- CreateTable
+-- CREATE TABLE "AssetEmbedding" (
+--     "id" TEXT NOT NULL,
+--     "assetId" TEXT NOT NULL,
+--     "model" TEXT NOT NULL,
+--     "contents" TEXT NOT NULL,
+--     "embedding" vector(1024) NOT NULL,
+
+--     CONSTRAINT "AssetEmbedding_pkey" PRIMARY KEY ("id")
+-- );
+
+-- -- AddForeignKey
+-- ALTER TABLE "AssetEmbedding" ADD CONSTRAINT "AssetEmbedding_assetId_fkey" FOREIGN KEY ("assetId") REFERENCES "Asset"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- -- AddIndex
+-- CREATE INDEX ON "AssetEmbedding" USING vectors (embedding vectors.vector_l2_ops);
+
 -- CreateTable
 CREATE TABLE "AssetEmbedding" (
     "id" TEXT NOT NULL,
     "assetId" TEXT NOT NULL,
-    "model" TEXT NOT NULL,
     "contents" TEXT NOT NULL,
+    "model" TEXT NOT NULL,
     "embedding" vector(1024) NOT NULL,
 
     CONSTRAINT "AssetEmbedding_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateIndex
+
+CREATE INDEX "AssetEmbedding_embedding_idx" ON "AssetEmbedding" USING hnsw (embedding vector_l2_ops);
+
 -- AddForeignKey
 ALTER TABLE "AssetEmbedding" ADD CONSTRAINT "AssetEmbedding_assetId_fkey" FOREIGN KEY ("assetId") REFERENCES "Asset"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddIndex
-CREATE INDEX ON "AssetEmbedding" USING vectors (embedding vectors.vector_l2_ops);
