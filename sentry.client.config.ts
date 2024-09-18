8e9a61eb2ab148803e92decf29f80afc022dd435
Nicolas Casco <nico@llamaworkspace.ai>
2024-09-18 12:58:41 +0200

    Add sentry (#304)

---
+++ sentry.client.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the client.
+// The config you add here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import { env } from '@/env.mjs'
+import * as Sentry from '@sentry/nextjs'
+
+function sentryInit() {
+  Sentry.init({
+    dsn: env.NEXT_PUBLIC_SENTRY_DSN,
+
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
+  })
+}
+
+if (env.NEXT_PUBLIC_SENTRY_DSN) {
+  sentryInit()
+}
