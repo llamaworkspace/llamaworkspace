4dcbbd2e17994fdad5d14e82bdc16fa6e66de17c
Nicolas Casco <nico@llamaworkspace.ai>
2024-09-20 11:14:17 +0200

    del example

--- src/website_example/WebsiteHome/WebsiteHomeFeatureWrapper.tsx
+++
@@ -1,5 +0,0 @@
-import type { PropsWithChildren } from 'react'
-
-export function WebsiteHomeFeatureWrapper({ children }: PropsWithChildren) {
-  return <div className="mx-auto max-w-6xl overflow-hidden">{children}</div>
-}
