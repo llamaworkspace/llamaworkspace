12338477c3ed5c9a1524e481c75a064e9006c639
Isaac Mayolas <imayolas@gmail.com>
2024-10-04 15:52:08 +0200

    db upgrade

---
+++ prisma/migrations/20241004135019_add_assetembedding/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "AssetEmbedding" (
+    "id" TEXT NOT NULL,
+    "assetId" TEXT NOT NULL,
+    "contents" TEXT NOT NULL,
+    "model" TEXT NOT NULL,
+    "embedding" vector(1024) NOT NULL,
+
+    CONSTRAINT "AssetEmbedding_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "AssetEmbedding_assetId_idx" ON "AssetEmbedding"("assetId");
+
+-- CreateIndex
+CREATE INDEX "AssetEmbedding_embedding_idx" ON "AssetEmbedding"("embedding");
+-- This is an empty migration.
