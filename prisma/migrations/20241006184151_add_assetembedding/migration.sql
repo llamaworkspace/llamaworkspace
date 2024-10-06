954dbc85ac41b9043d7b7a24c791853dfae0afeb
Isaac Mayolas <imayolas@gmail.com>
2024-10-06 22:46:48 +0200

    fix

--- prisma/migrations/20241006184151_add_assetembedding/migration.sql
+++ prisma/migrations/20241006184151_add_assetembedding/migration.sql
@@ -9,6 +9,10 @@ CREATE TABLE "AssetEmbedding" (
     CONSTRAINT "AssetEmbedding_pkey" PRIMARY KEY ("id")
 );
 
+-- AddForeignKey
+ALTER TABLE "AssetEmbedding" ADD CONSTRAINT "AssetEmbedding_assetId_fkey" FOREIGN KEY ("assetId") REFERENCES "Asset"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+
 -- CreateIndex
 CREATE INDEX "AssetEmbedding_assetId_idx" ON "AssetEmbedding"("assetId");
 
