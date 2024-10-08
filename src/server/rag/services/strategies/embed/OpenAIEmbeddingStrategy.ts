44658b2c92efdfb7d0a6ca2d8fb2fc523fff2ac8
Isaac Mayolas <imayolas@gmail.com>
2024-10-08 17:53:07 +0200

    fix·

--- src/server/rag/services/strategies/embed/OpenAIEmbeddingStrategy.ts
+++ src/server/rag/services/strategies/embed/OpenAIEmbeddingStrategy.ts
@@ -2,15 +2,15 @@ import { DEFAULT_EMBEDDING_MODEL } from '@/server/rag/ragConstants'
 import type { Document } from '@langchain/core/documents'
 import { OpenAIEmbeddings } from '@langchain/openai'
 
-const embeddingsOai = new OpenAIEmbeddings({
-  verbose: true,
-  apiKey: process.env.INTERNAL_OPENAI_API_KEY,
-  modelName: DEFAULT_EMBEDDING_MODEL.replace('openai/', ''),
-  dimensions: 1024,
-})
-
 export class OpenAIEmbeddingStrategy {
   async embed(documents: Document[]) {
+    const embeddingsOai = new OpenAIEmbeddings({
+      verbose: true,
+      apiKey: process.env.INTERNAL_OPENAI_API_KEY,
+      modelName: DEFAULT_EMBEDDING_MODEL.replace('openai/', ''),
+      dimensions: 1024,
+    })
+
     return await embeddingsOai.embedDocuments(
       documents.map((doc) => doc.pageContent),
     )
