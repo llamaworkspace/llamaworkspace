7d07a06272e11ce846cb6da5c43bf2121a13738e
Isaac Mayolas <imayolas@gmail.com>
2024-10-20 13:29:28 +0200

    wip

--- src/website/WebsiteFooterCTA.tsx
+++ src/website/WebsiteFooterCTA.tsx
@@ -5,13 +5,13 @@ export function WebsiteFooterCTA() {
     <div className="bg-zinc-900 py-32 text-white">
       <div className="px-6 sm:px-6">
         <div className="mx-auto max-w-2xl text-center">
-          <h2 className="text-3xl font-semibold tracking-tight  sm:text-4xl">
+          <h2 className="font-heading text-3xl font-semibold tracking-tight  sm:text-4xl">
             Try Llama Workspace today
           </h2>
 
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <Link
-              href="/auth/signup"
+              href="/auth/signin"
               className="rounded-md bg-white px-6 py-4 font-semibold text-zinc-900 shadow-sm hover:bg-zinc-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-zinc-600"
             >
               Create an account
