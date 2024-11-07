e8cc1cdb391781c9921c344ae4a915f3634a0e53
Isaac Mayolas <imayolas@gmail.com>
2024-11-07 16:20:00 +0100

    Feature/tsconfig 2 (#340)

--- apps/main/.eslintrc.cjs
+++ apps/main/.eslintrc.cjs
@@ -2,7 +2,7 @@
 const config = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: true,
+    project: './tsconfig.json',
   },
   plugins: ['@typescript-eslint'],
   extends: [
