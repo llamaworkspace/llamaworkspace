0d68026ac13bdf63b9cef76ebcb2786fcdaf56e1
Isaac Mayolas <imayolas@gmail.com>
2024-11-14 18:31:40 +0100

    layout progress

--- src/app/p/layout.tsx
+++ src/app/p/layout.tsx
@@ -83,12 +83,83 @@ const AppHeader = () => {
 export function AppSidebar() {
   return (
     <Sidebar>
-      <SidebarHeader />
+      <SidebarHeader>
+        <ul data-sidebar="menu" className="flex w-full min-w-0 flex-col gap-1">
+          <li data-sidebar="menu-item" className="group/menu-item relative">
+            <button
+              data-sidebar="menu-button"
+              data-size="lg"
+              data-active="false"
+              className="peer/menu-button [&amp;>span:last-child]:truncate [&amp;>svg]:size-4 [&amp;>svg]:shrink-0 flex h-12 w-full items-center gap-2 overflow-hidden rounded-md p-2 text-left text-sm outline-none ring-sidebar-ring transition-[width,height,padding] hover:bg-sidebar-accent hover:text-sidebar-accent-foreground focus-visible:ring-2 active:bg-sidebar-accent active:text-sidebar-accent-foreground disabled:pointer-events-none disabled:opacity-50 group-has-[[data-sidebar=menu-action]]/menu-item:pr-8 aria-disabled:pointer-events-none aria-disabled:opacity-50 data-[active=true]:bg-sidebar-accent data-[state=open]:bg-sidebar-accent data-[active=true]:font-medium data-[active=true]:text-sidebar-accent-foreground data-[state=open]:text-sidebar-accent-foreground data-[state=open]:hover:bg-sidebar-accent data-[state=open]:hover:text-sidebar-accent-foreground group-data-[collapsible=icon]:!size-8 group-data-[collapsible=icon]:!p-0"
+              type="button"
+              id="radix-:Rcpuuuuu6ja:"
+              aria-haspopup="menu"
+              aria-expanded="false"
+              data-state="closed"
+            >
+              Button here
+            </button>
+          </li>
+        </ul>
+      </SidebarHeader>
       <SidebarContent>
         <SidebarGroup />
         <SidebarGroup />
       </SidebarContent>
-      <SidebarFooter />
+      <SidebarFooter>
+        <DaFooter />
+      </SidebarFooter>
     </Sidebar>
+  )
+}
+
+const DaFooter = () => {
+  return (
+    <div>
+      <div data-sidebar="footer" className="flex flex-col gap-2 p-2">
+        <ul data-sidebar="menu" className="flex w-full min-w-0 flex-col gap-1">
+          <li data-sidebar="menu-item" className="group/menu-item relative">
+            <button
+              data-sidebar="menu-button"
+              data-size="lg"
+              data-active="false"
+              className="peer/menu-button [&amp;>span:last-child]:truncate [&amp;>svg]:size-4 [&amp;>svg]:shrink-0 flex h-12 w-full items-center gap-2 overflow-hidden rounded-md p-2 text-left text-sm outline-none ring-sidebar-ring transition-[width,height,padding] hover:bg-sidebar-accent hover:text-sidebar-accent-foreground focus-visible:ring-2 active:bg-sidebar-accent active:text-sidebar-accent-foreground disabled:pointer-events-none disabled:opacity-50 group-has-[[data-sidebar=menu-action]]/menu-item:pr-8 aria-disabled:pointer-events-none aria-disabled:opacity-50 data-[active=true]:bg-sidebar-accent data-[state=open]:bg-sidebar-accent data-[active=true]:font-medium data-[active=true]:text-sidebar-accent-foreground data-[state=open]:text-sidebar-accent-foreground data-[state=open]:hover:bg-sidebar-accent data-[state=open]:hover:text-sidebar-accent-foreground group-data-[collapsible=icon]:!size-8 group-data-[collapsible=icon]:!p-0"
+              type="button"
+              id="radix-:Rspuuuuu6ja:"
+              aria-haspopup="menu"
+              aria-expanded="false"
+              data-state="closed"
+            >
+              <span className="relative flex h-8 w-8 shrink-0 overflow-hidden rounded-lg">
+                <img
+                  className="aspect-square h-full w-full"
+                  alt="shadcn"
+                  src="/avatars/shadcn.jpg"
+                />
+              </span>
+              <div className="grid flex-1 text-left text-sm leading-tight">
+                <span className="truncate font-semibold">shadcn</span>
+                <span className="truncate text-xs">m@example.com</span>
+              </div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="lucide lucide-chevrons-up-down ml-auto size-4"
+              >
+                <path d="m7 15 5 5 5-5"></path>
+                <path d="m7 9 5-5 5 5"></path>
+              </svg>
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
   )
 }
